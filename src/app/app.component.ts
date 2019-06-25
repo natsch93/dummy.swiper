573762f9f7b0eab3559b22fa74bbb6ed7df5ddde
Nathaniel Schembri <nathanielschembri@hotmail.com>
2019-06-25 13:23:56 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { ViewportService, ViewportSizeType } from '@ssv/ngx.ux';
+import { tap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dummy-swiper';
+export class AppComponent implements OnInit {
+
+	isViewportSmall: boolean;
+	config: SwiperConfigInterface = {
+		loop: true,
+		observer: true
+	};
+	items: string[] = [
+		'item 1',
+		'item 2'
+	];
+
+	constructor(
+		private viewportService: ViewportService,
+	) { }
+
+	ngOnInit() {
+		this.viewportService.sizeType$.pipe(
+			tap(x => this.isViewportSmall = x.type <= ViewportSizeType.small),
+			tap(() => console.warn(">>>> isVPSmall", this.isViewportSmall))
+		).subscribe();
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SwiperModule } from 'ngx-swiper-wrapper';
 
 import { AppComponent } from './app.component';
+import { SsvUxModule } from '@ssv/ngx.ux';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		SwiperModule,
+		SsvUxModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
