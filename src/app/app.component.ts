07b8a19b7de4c2de32613b96471e2547b8700f15
Nathaniel Schembri <nathanielschembri@hotmail.com>
2020-02-13 13:38:09 +0100

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 import { ViewportService, ViewportSizeType } from '@ssv/ngx.ux';
 import { tap } from 'rxjs/operators';
 
+// tslint:disable
+
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
@@ -10,15 +12,34 @@ import { tap } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit {
 
+	// 9
 	isViewportSmall: boolean;
 	config: SwiperConfigInterface = {
 		loop: true,
-		observer: true,
-		navigation: true
+		// observer: true,
+		navigation: true,
+		// slidesPerGroup: 3,
+		slidesPerView: 3,
+		spaceBetween: 20,
+		// loopAdditionalSlides: 3
+		// loopedSlides: 0
+		pagination: {
+			type: "bullets",
+			clickable: true
+		}
+
 	};
 	items: string[] = [
 		'item 1',
-		'item 2'
+		'item 2',
+		'item 3',
+		'item 4',
+		'item 5',
+		'item 6',
+		'item 7',
+		'item 8',
+		'item 9',
+		'item 10',
 	];
 
 	constructor(
